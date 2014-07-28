8ab8dfbf2c8da2035fe881965d1c48b64f942555
Bolutife Ogunsola <bolutife@codility.com>
2014-07-28 14:20:59 +0100

    Implement customizable positioning for number
    Basically, make it possible to place the number in any
    of the four corners
    TODO: Update README to describe how to use

--- intro.js
+++ intro.js
@@ -126,7 +126,8 @@
             intro: currentElement.getAttribute('data-intro'),
             step: parseInt(currentElement.getAttribute('data-step'), 10),
             tooltipClass: currentElement.getAttribute('data-tooltipClass'),
-            position: currentElement.getAttribute('data-position') || this._options.tooltipPosition
+            position: currentElement.getAttribute('data-position') || this._options.tooltipPosition,
+            numberPosition: currentElement.getAttribute('data-numberPosition')
           };
         }
       }
@@ -152,7 +153,8 @@
             intro: currentElement.getAttribute('data-intro'),
             step: nextStep + 1,
             tooltipClass: currentElement.getAttribute('data-tooltipClass'),
-            position: currentElement.getAttribute('data-position') || this._options.tooltipPosition
+            position: currentElement.getAttribute('data-position') || this._options.tooltipPosition,
+            numberPosition: currentElement.getAttribute('data-numberPosition')
           };
         }
       }
@@ -412,6 +414,14 @@
     }
 
     tooltipLayer.className = ('introjs-tooltip ' + tooltipCssClass).replace(/^\s+|\s+$/g, '');
+    
+    //get number position for element
+    var currentNumberPosition = this._introItems[this._currentStep].numberPosition;
+    helperNumberLayer.className = 'introjs-helperNumberLayer';
+    if (currentNumberPosition === undefined){
+      currentNumberPosition = 'left';
+    }
+    helperNumberLayer.classList.add(currentNumberPosition);
 
     //custom css class for tooltip boxes
     var tooltipCssClass = this._options.tooltipClass;
@@ -424,11 +434,14 @@
         arrowLayer.className = 'introjs-arrow bottom';
         break;
       case 'right':
+        if (this._options.showStepNumbers == true && (currentNumberPosition === 'right' || currentNumberPosition === 'top-right')) {
+          tooltipLayer.style.top = '15px';
+        }
         tooltipLayer.style.left = (_getOffset(targetElement).width + 20) + 'px';
         arrowLayer.className = 'introjs-arrow left';
         break;
       case 'left':
-        if (this._options.showStepNumbers == true) {
+        if (this._options.showStepNumbers == true && (currentNumberPosition === 'left' || currentNumberPosition === 'top-left')) {
           tooltipLayer.style.top = '15px';
         }
         tooltipLayer.style.right = (_getOffset(targetElement).width + 20) + 'px';
@@ -453,7 +466,13 @@
         break;
       case 'bottom-right-aligned':
         arrowLayer.className      = 'introjs-arrow top-right';
-        tooltipLayer.style.right  = '0px';
+        
+        if (currentNumberPosition === 'bottom-right') {
+          tooltipLayer.style.right  = '15px';
+        }
+        else{
+          tooltipLayer.style.right  = '0x';
+        }
         tooltipLayer.style.bottom = '-' + (_getOffset(tooltipLayer).height + 10) + 'px';
         break;
       case 'bottom-middle-aligned':
@@ -469,6 +488,9 @@
       case 'bottom':
       // Bottom going to follow the default behavior
       default:
+        if (currentNumberPosition === 'bottom' || currentNumberPosition === 'bottom-left') {
+          tooltipLayer.style.left  = '15px';
+        }
         tooltipLayer.style.bottom = '-' + (_getOffset(tooltipLayer).height + 10) + 'px';
         arrowLayer.className = 'introjs-arrow top';
         break;
