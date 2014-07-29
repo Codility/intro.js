6cebbf8118c095741f6e87b8043a4d5d83ab7585
Bolutife Ogunsola <bolutife@codility.com>
2014-07-29 16:44:22 +0100

    Remove window resize handlers when exiting introJS

--- intro.js
+++ intro.js
@@ -404,8 +404,10 @@
     //clean listeners
     if (window.removeEventListener) {
       window.removeEventListener('keydown', this._onKeyDown, true);
+      window.removeEventListener("resize", this._onResize, true);
     } else if (document.detachEvent) { //IE
       document.detachEvent('onkeydown', this._onKeyDown);
+      document.detachEvent("onresize", this._onResize);
     }
 
     //set the step to zero
